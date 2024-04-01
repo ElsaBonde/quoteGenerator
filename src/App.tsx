84a721e68c39c08fb12bb219c69b6db965262a90
Elsa Bonde <elsasofiabonde@gmail.com>
2024-04-01 19:55:29 +0200

    Added tailwind and wrote first draft of app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Quote Generator</h1>
+      </header>
+      <main>
+        <section>
+          <p>Here is a quote</p>
+        </section>
+        <section>
+          <h2>
+            Author: <span>Here is the author</span>
+          </h2>
+        </section>
+        <button>Generate new Quote</button>
+      </main>
+      <footer>
+        <p>Created by @ElsaBonde</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
