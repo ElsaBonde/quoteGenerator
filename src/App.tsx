ac4d4da34d3002236cb57ccc4e3bc51704a081aa
Elsa Bonde <elsasofiabonde@gmail.com>
2024-04-01 21:31:17 +0200

    added function that doesn't do anything at all

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  //Namnger variabler på ett extremt dåligt och otydligt sätt, bättre hade varit att använda sig av namnen quote, setQuote och author, setAuthor
+  //namnger variabler på ett extremt dåligt och otydligt sätt, bättre hade varit att använda sig av namnen quote, setQuote och author, setAuthor
   const [q, sQ] = useState("");
   const [a, sA] = useState("");
 
-  //Dåligt namn på funktion, bättre hade varit att använda sig av något mer beskrivande som fetchQuote, fetchRandomQuote eller liknande. Dessutom är namnet felaktigt skrivet, det ska vara retrieveData (camelCase) för att hålla koden konsekvent och öka läsbarheten.
+  //dåligt namn på funktion, bättre hade varit att använda sig av något mer beskrivande som fetchQuote, fetchRandomQuote eller liknande. dessutom är namnet felaktigt skrivet, det ska vara retrieveData (camelCase) för att hålla koden konsekvent och öka läsbarheten.
   const RetriveData = () => {
     fetch("https://api.quotable.io/random")
       .then((response) => {
@@ -22,13 +22,17 @@ function App() {
       })
 
       .catch((error) => {
-        //Det hade varit bättre att använda console.error än console.log för att göra användaren uppmärksam på att det är ett error.
+        //det hade varit bättre att använda console.error än console.log för att göra användaren uppmärksam på att det är ett error.
         console.log("There was a problem with the fetch operation:", error);
-        //Hade varit bättre att använda exempelvis SetError för tyfligt visa användaren i användargränssnittet att ett error uppstått.
+        //hade varit bättre att använda exempelvis SetError för tyfligt visa användaren i användargränssnittet att ett error uppstått.
         alert(
           "There was a problem fetching the quote. Please try again later."
         );
       });
+  };
+
+  //en helt onödig funktion som inte används någonstans i koden vilket kommer förvirra andra utvecklare som läser koden. man borde ta bort funktionen istället.
+  const doThatThing = () => {
   };
 
   useEffect(() => {
