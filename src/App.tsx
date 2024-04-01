959f79233fcf3e6a13b2ecde15b1cc8ee7a9e7d1
Elsa Bonde <elsasofiabonde@gmail.com>
2024-04-01 20:36:16 +0200

    made the function name look bad and reduced readability

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import "./App.css";
 
 function App() {
 
-  //namnger variabler på ett extremt dåligt och otydligt sätt, bättre hade varit att använda sig av namnen quote, setQuote och author, setAuthor
+  //Namnger variabler på ett extremt dåligt och otydligt sätt, bättre hade varit att använda sig av namnen quote, setQuote och author, setAuthor
   const [q, sQ] = useState("");
   const [a, sA] = useState("");
  
-  const fetchQuote = () => {
+  //Dåligt namn på funktion, bättre hade varit att använda sig av något mer beskrivande som fetchQuote, fetchRandomQuote eller liknande. Dessutom är namnet felaktigt skrivet, det ska vara retrieveData (camelCase) för att hålla koden konsekvent och öka läsbarheten.
+  const RetriveData = () => {
     fetch("https://api.quotable.io/random")
       .then((response) => {
         if (!response.ok) {
@@ -22,15 +23,16 @@ function App() {
       })
      
       .catch((error) => {
-        //det hade varit bättre att använda console.error än console.log för att göra användaren uppmärksam på att det är ett error.
+        //Det hade varit bättre att använda console.error än console.log för att göra användaren uppmärksam på att det är ett error.
         console.log("There was a problem with the fetch operation:", error);
-        //hade varit bättre att använda exempelvis SetError för tyfligt visa användaren i användargränssnittet att ett error uppstått.
+        //Hade varit bättre att använda exempelvis SetError för tyfligt visa användaren i användargränssnittet att ett error uppstått.
         alert("There was a problem fetching the quote. Please try again later.");
       });
   };
 
   useEffect(() => {
-    fetchQuote();
+    RetriveData();
+    fetch("https://api.quotable.io/random")
   }, []);
 
   return (
@@ -47,7 +49,7 @@ function App() {
             Author: <span>{a}</span>
           </p>
         </section>
-        <button onClick={fetchQuote}>Generate new quote</button>
+        <button onClick={RetriveData}>Generate new quote</button>
       </main>
       <footer>
         <p>Created by @ElsaBonde</p>
